9b602f63ec860c0568516efad91dec50001a9935
yutao chen <yutao.chen@pharmaron-bj.com>
2026-02-26 08:40:40 +0800

    Add snake game

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
+    headers: async () => {
+        return [
+            {
+                source: '/nbtv.m3u',
+                headers: [
+                    { key: 'Content-Type', value: 'text/plain' }
+                ]
+            }
+        ]
+    },
 };
 
 export default nextConfig;
